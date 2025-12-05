4d68985260a1cb9c56b7a58677579e99151e09e3
white-star <wh173-star@protonmail.com>
2025-12-05 23:24:07 +0900

    Change the deterministic solve.

--- Nonogram Solver Core/Algorithm/DeterministicSolve/DeterministicSolve/DeterministicSolve.h
+++ Nonogram Solver Core/Algorithm/DeterministicSolve/DeterministicSolve/DeterministicSolve.h
@@ -2,21 +2,18 @@
 #define DETERMINISTICSOLVE_H
 
 
-#include <atomic>
-#include <thread>
 #include "SharedBacktrackBoard.h"
 #include "SharedBacktrackStack.h"
 #include "SharedHighlightIndexes.h"
+#include "IDeterministicSolve.h"
 #include "Line.h"
 #include "HintSet.h"
 
-class BacktrackAlgorithm {
+class BacktrackAlgorithm : public IDeterministicSolve {
 private:
 	SharedBacktrackBoard& sharedBacktrackBoard;
 	SharedBacktrackStack& sharedBacktrackStack;
 	SharedHighlightIndexes& sharedHighlightIndexes;
-    std::atomic<bool> terminate{ false };
-	std::vector<Board> solutions;
 
 public:
     explicit BacktrackAlgorithm(
@@ -25,15 +22,7 @@ public:
 		SharedHighlightIndexes& sharedHighlightIndexes
     );
 
-	void run();
-
-	void backtrackSolve();
-	void backtrackSolveRecursive(int depth);
-
-	bool deterministicSolve(int waitMillis);
-
-    bool waitAndCheckTermination(const int waitMillis) const;
-    void request_stop();
+	bool deterministicSolve(int waitMillis) override
 };
 
 
