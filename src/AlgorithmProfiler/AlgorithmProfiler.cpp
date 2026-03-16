430aaee3897b2dcb20e6c693a6a4951eda4512d7
white-star <wh173-star@protonmail.com>
2026-03-16 21:55:34 +0900

    Add the very simple profiler class to check the time and memory limit exceeding.

---
+++ src/AlgorithmProfiler/AlgorithmProfiler.cpp
@@ -0,0 +1,30 @@
+#include "AlgorithmProfiler.h"
+
+AlgorithmProfiler::AlgorithmProfiler(double timeLimitMs, size_t stackLimitBytes)
+    : timeLimitMilliseconds(timeLimitMs)
+    , stackUsageLimitBytes(stackLimitBytes)
+    , stackBaseAddress(0) {}
+
+void AlgorithmProfiler::startMeasurement() {
+    char localVariableForStackReference;
+    stackBaseAddress = reinterpret_cast<uintptr_t>(&localVariableForStackReference);
+    startTime = std::chrono::high_resolution_clock::now();
+}
+
+bool AlgorithmProfiler::isTimeLimitExceeded() const {
+    auto currentTime = std::chrono::high_resolution_clock::now();
+    double elapsedDuration =
+      std::chrono::duration<double, std::milli>(currentTime - startTime).count();
+    return elapsedDuration > timeLimitMilliseconds;
+}
+
+bool AlgorithmProfiler::isStackUsageLimitExceeded() const {
+    char currentLocalVariable;
+    uintptr_t currentStackPointer = reinterpret_cast<uintptr_t>(&currentLocalVariable);
+
+    size_t currentUsage = (stackBaseAddress > currentStackPointer)
+                            ? (stackBaseAddress - currentStackPointer)
+                            : (currentStackPointer - stackBaseAddress);
+
+    return currentUsage > stackUsageLimitBytes;
+}
