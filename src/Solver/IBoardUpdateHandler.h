dde80304de16cd56bacf46917a7bf69244736c66
white-star <wh173-star@protonmail.com>
2026-03-30 22:11:10 +0900

    Change the BoardUpdateHandler.

--- src/Solver/IBoardUpdateHandler.h
+++ src/Solver/IBoardUpdateHandler.h
@@ -2,13 +2,30 @@
 #define IBOARDUPDATEHANDLER_H
 
 #include "Board/Board/Board.h"
+#include "Index/Position/CellPosition/CellPosition.h"
+#include "Index/Position/HintPosition/HintPosition.h"
+#include "Index/Position/LinePosition/LinePosition.h"
 
 class IBoardUpdateHandler {
   public:
     virtual ~IBoardUpdateHandler() = default;
-    virtual void onCellUpdate(const Board& board) = 0;
-    virtual void onLineUpdate(const Board& board) = 0;
-    virtual void onBoardUpdate(const Board& board) = 0;
+
+    virtual void onUpdate() = 0;
+
+    virtual void onCellUpdate(
+      const CellPosition &cellPosition,
+      const Cell &targetCell,
+      const Cell &beforeCell,
+      const Cell &afterCell) = 0;
+
+    virtual void onLineUpdate(
+      const LinePosition &linePosition,
+      const Line &targetLine,
+      const Line &beforeLine,
+      const Line &afterLine) = 0;
+
+    virtual void
+    onBoardUpdate(const Board &targetBoard, const Board &beforeBoard, const Board &afterBoard) = 0;
 };
 
 #endif
