0b1bfdad890ad2ea542988b510ca4d7bddf7995f
white-star <wh173-star@protonmail.com>
2026-04-29 20:22:33 +0900

    Delete classes to wrap an integer.

--- src/Core/Board/BoardLength/BoardLength.cpp
+++
@@ -1,9 +0,0 @@
-#include "Core/Board/BoardLength/BoardLength.h"
-
-namespace VersaNo::Core {
-
-BoardLength::BoardLength(int length) : length(length) {}
-
-int BoardLength::getLength() const { return length; }
-
-} // namespace VersaNo::Core
