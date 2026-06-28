c1cdef3a6fb6af9fe947a4d8fab08320610b8a72
white-star <wh173-star@protonmail.com>
2026-06-28 20:14:35 +0900

    Add IBoard.

---
+++ src/Core/Board/Board/IBoard.h
@@ -0,0 +1,44 @@
+#ifndef VERSANO_CORE_IBOARD_H
+#define VERSANO_CORE_IBOARD_H
+
+#include "Core/Cell/CellColor/CellColor.h"
+#include "Core/Cell/CellVector2D/CellVector2D.h"
+#include "Core/Position/CellPosition/CellPosition.h"
+#include "Core/Position/HintPosition/HintPosition.h"
+#include "Core/Position/LinePosition/LinePosition.h"
+#include "Core/Types/AppliedType/AppliedType.h"
+#include <vector>
+
+namespace VersaNo::Core {
+
+class IBoard {
+  public:
+    RowLength getRowLength() const = 0;
+    ColumnLength getColumnLength() const = 0;
+    Cell getCell(CellPosition coordinate) const = 0;
+
+    Row getRow(RowIndex index) const = 0;
+    Column getColumn(ColumnIndex index) const = 0;
+
+    void applyCell(CellPosition coordinate, const Cell &cell, bool overrideNone = false);
+
+    void applyRow(LinePosition<RowOrientation> linePosition, const Row &row, bool overwriteNone);
+    void applyColumn(
+      LinePosition<ColumnOrientation> linePosition, const Column &column, bool overwriteNone);
+
+    template <typename TOrientation>
+    void applyPlacement(
+      typename LinePosition<TOrientation> linePosition,
+      const typename LineTraits<TOrientation>::Placement &placement);
+
+    template <typename TOrientation>
+    void applyHint(typename HintPosition<TOrientation> hintPosition, HintNumber hintNumber);
+
+    void applyBoard(const IBoard &board, bool overwriteNone);
+
+    bool isSolved() const;
+};
+
+} // namespace VersaNo::Core
+
+#endif // VERSANO_CORE_IBOARD_H
